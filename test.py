b91d5f3969bb614883bb2c143d2924c916529a3a
stella <stellabasovsky@gmail.com>
2019-09-09 12:17:29 +0300

    changint the py test

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from selenium import webdriver
 
 
 driver = webdriver.Chrome(executable_path="/Users/stella/Documents/chromedriver")
-driver.get("http://192.168.99.100:5000/")
+driver.get("http://localhost:8080")
