acff7cf475d46bbff5db7ac4dddd3684cc983111
stella <stellabasovsky@gmail.com>
2019-09-09 15:49:04 +0300

    changed the port to 5000

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ from selenium import webdriver
 
 
 driver = webdriver.Chrome(executable_path="/Users/stella/Documents/chromedriver")
-driver.get("http://localhost:8080")
+driver.get("http://localhost:5000")
